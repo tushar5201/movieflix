1e218b6354fe552d2b2a8b5220abcfb42979058d
Tushar5201 <tusharlakadiya@gmail.com>
2024-05-15 16:29:49 +0530

    server cors commit

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 app.use(cors(
     {
-        origin: ["https://movieflix-rho.vercel.app/"],
+        origin: ["https://movieflix-zzmw.vercel.app/"],
         methods: ["POST", "GET", "DELETE", "PUT"],
         ceadentials: "true"
     }
