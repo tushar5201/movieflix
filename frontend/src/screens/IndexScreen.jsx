fe60972dacada34c6f2e86b2c219d1d95168ce27
Tushar5201 <tusharlakadiya@gmail.com>
2024-05-18 11:39:59 +0530

    proxy commit

--- frontend/src/screens/IndexScreen.jsx
+++ frontend/src/screens/IndexScreen.jsx
@@ -11,6 +11,7 @@ import MessageBox from '../components/MessageBox';
 import { getError } from '../utils';
 // import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
+import { Tilt } from 'react-tilt'
 // import { Store } from '../Store.js';
 // import Category from '../category.jsx';
 
@@ -105,7 +106,17 @@ export default function IndexScreen() {
         }
     }
 
-    // document.getElementsByClassName(".slick-next").style.border = "2px solid #fff"
+    const defaultOptions = {
+        reverse: false,  // reverse the tilt direction
+        max: 35,     // max tilt rotation (degrees)
+        perspective: 1000,   // Transform perspective, the lower the more extreme the tilt gets.
+        scale: 1.1,    // 2 = 200%, 1.5 = 150%, etc..
+        speed: 1000,   // Speed of the enter/exit transition
+        transition: true,   // Set a transition on enter/exit.
+        axis: null,   // What axis should be disabled. Can be X or Y.
+        reset: true,    // If the tilt effect has to be reset on exit.
+        easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+    }
 
     return (
         <div className='body'>
@@ -116,10 +127,12 @@ export default function IndexScreen() {
                     {
                         loading ? <LoadingBox /> : error ? <MessageBox variant='danger'>{error}</MessageBox> : (
                             movies.slice(0).reverse().map((movie) => (
-                                <Link className='overlay1' key={movie._id} to={`/movie/${movie._id}`}>
-                                    <img src={`/admin/get-movie-image/${movie._id}`} alt={movie.title} className='slider_img' />
-                                    <div className='overlay'>{movie.name}<br />{movie.year}</div>
-                                </Link>
+                                <Tilt defaultOptions>
+                                    <Link className='overlay1' key={movie._id} to={`/movie/${movie._id}`}>
+                                        <img src={`/admin/get-movie-image/${movie._id}`} alt={movie.title} className='slider_img' />
+                                        <div className='overlay'>{movie.name}<br />{movie.year}</div>
+                                    </Link>
+                                </Tilt>
                             )))}
                 </Slider>
 
