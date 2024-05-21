cc5e9eb7cbc6dfe91eedc49b562a65302c81c638
Tushar5201 <tusharlakadiya@gmail.com>
2024-05-21 16:50:22 +0530

    series commit

--- frontend/src/screens/IndexScreen.jsx
+++ frontend/src/screens/IndexScreen.jsx
@@ -92,7 +92,9 @@ export default function IndexScreen() {
                 </Slider>
 
                 <h4 className='titles'>&nbsp;Popular series on Movieflix</h4>
-                {/* <AllSeries /> */}
+                <Slider className='slider' {...settings}>
+                    <AllSeries />
+                </Slider>
             </div>
         </div>
     )
