9a98905924398c1ba92595347a357a890541c9f7
Tushar5201 <tusharlakadiya@gmail.com>
2024-05-23 09:28:01 +0530

    auth commit

--- backend/server.js
+++ backend/server.js
@@ -142,7 +142,7 @@ app.post('/sign_in', async (req, res) => {
         if (isMatch) {
             // const token = await user.generateAuthToken();
             // console.log(token);
-            const token = jwt.sign(user, 'hellofuckyou', {
+            const token = jwt.sign(email, 'hellofuckyou', {
                 expiresIn: 1800000
             })
             res.cookie("movieflixToken", token, { maxAge: 1800000 });
