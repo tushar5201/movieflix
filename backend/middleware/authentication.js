43842371d78daa49174eb97c6974f8d806868a1c
Tushar5201 <tusharlakadiya@gmail.com>
2024-05-27 08:44:30 +0530

    auth1 commit

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -20,7 +20,7 @@ const authentication = async (req, res, next) => {
     //     console.log(err);
     // }.
 
-    const token = req.cookies.movieflixToken;
+    const token = req.cookies;
     if (!token) {
         return res.sendStatus(403);
     }
