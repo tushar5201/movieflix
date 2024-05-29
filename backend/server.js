6681b21c5cf3dc88a19583a9a0909a5ea32899f8
Tushar5201 <tusharlakadiya@gmail.com>
2024-05-29 11:38:10 +0530

    auth1 commit

--- backend/server.js
+++ backend/server.js
@@ -156,8 +156,9 @@ app.post("/sign_in", async (req, res) => {
     if (user) {
         if (bcrypt.compareSync(password, user.password)) {
             const jwtToken = jwt.sign({ email: user.email }, process.env.SECRET_KEY);
+            document.cookie = "jwtToken=" + jwtToken 
             return res
-                .cookie("jwtToken", jwtToken, { httpOnly: false, sameSite: "none", secure: true })
+                // .cookie("jwtToken", jwtToken, { httpOnly: false, sameSite: "none", secure: true })
                 .status(200)
                 .send({ email: user.email, isAdmin: user.isAdmin, name: user.name });
         } else {
