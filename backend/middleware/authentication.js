51e94a52fb814fd3d6e41285fe979470fe7d33b0
Tushar5201 <tusharlakadiya@gmail.com>
2024-05-29 14:17:28 +0530

    auth commit

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -2,38 +2,41 @@ import jwt from "jsonwebtoken";
 import Users from "../models/userModel.js"
 
 const authentication = async (req, res, next) => {
+    // try {
+    //     const token = req.cookies.movieflixToken;
+    //     if (token) {
+    //         const verifyToken = jwt.verify(token, 'hellofuckyou');
+    //         const rootUser = await Users.findOne({ _id: verifyToken._id });
+
+    //         if (!rootUser) { throw new Error('User not found') }
+
+    //         req.rootUser = rootUser;
+    //     } else {
+    //         res.status(403).send('Token not found')
+    //     }
+    //     next();
+    // } catch (err) {
+    //     res.status(401).send('Unauthorized token.')
+    //     console.log(err);
+    // }
     try {
-        const token = req.cookies;
+        const token = req.cookies.jwtToken;
         if (token) {
-            const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
-            const rootUser = await Users.findOne({ _id: verifyToken._id });
-
-            if (!rootUser) { throw new Error('User not found') }
-
-            req.rootUser = rootUser;
+            const data = jwt.verify(token, process.env.SECRET_KEY);
+            if (data) {
+                const rootUser = await Users.findOne({ email: data.email });
+                if (!rootUser) { return res.sendStatus(410).send('User not found') }
+                req.rootUser = rootUser;
+                next();
+            } else {
+                return res.sendStatus(401)
+            }
         } else {
-            res.status(403).send('Token not found')
+            return res.sendStatus(403);
         }
-        next();
     } catch (err) {
-        res.status(401).send('Unauthorized token.')
         console.log(err);
     }
-
-    // const token = req.cookies.jwtToken;
-    // if (token) {
-    //     const data = jwt.verify(token, process.env.SECRET_KEY);
-    //     if (data) {
-    //         const rootUser = await Users.findOne({ email: data.email });
-    //         if (!rootUser) { return res.sendStatus(410).send('User not found') }
-    //         req.rootUser = rootUser;
-    //         next();
-    //     } else {
-    //         return res.sendStatus(401)
-    //     }
-    // } else {
-    //     return res.sendStatus(403);
-    // }
 }
 
 export default authentication;
