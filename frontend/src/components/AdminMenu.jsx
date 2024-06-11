855569050d63a1787814216001cf9a1a61a18719
Tushar5201 <tusharlakadiya@gmail.com>
2024-06-11 09:16:18 +0530

    admin menu

--- frontend/src/components/AdminMenu.jsx
+++ frontend/src/components/AdminMenu.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function AdminMenu() {
   return (
@@ -13,48 +13,48 @@ export default function AdminMenu() {
           <NavLink to='/dashboard/categories' className='list-group-item list-group-item-action'>Categories</NavLink>
         </div>
       </div> */}
-      <div className="d-flex flex-column flex-shrink-0 bg-light" style="width: 4.5rem;">
-        <a href="/" className="d-block p-3 link-dark text-decoration-none" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Icon-only">
+      <div className="d-flex flex-column flex-shrink-0 bg-light" style={{ width: "4.5rem" }}>
+        <Link href="/" className="d-block p-3 link-dark text-decoration-none" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Icon-only">
           <svg className="bi" width="40" height="32"></svg>
           <span className="visually-hidden">Icon-only</span>
-        </a>
+        </Link>
         <ul className="nav nav-pills nav-flush flex-column mb-auto text-center">
           <li className="nav-item">
-            <a href="#" className="nav-link active py-3 border-bottom" aria-current="page" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
+            <Link href="#" className="nav-link active py-3 border-bottom" aria-current="page" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
               <svg className="bi" width="24" height="24" role="img" aria-label="Home"></svg>
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#" className="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
+            <Link href="#" className="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
               <svg className="bi" width="24" height="24" role="img" aria-label="Dashboard"></svg>
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#" className="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Orders">
+            <Link href="#" className="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Orders">
               <svg className="bi" width="24" height="24" role="img" aria-label="Orders"></svg>
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#" className="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Products">
+            <Link href="#" className="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Products">
               <svg className="bi" width="24" height="24" role="img" aria-label="Products"></svg>
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#" className="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Customers">
+            <Link href="#" className="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Customers">
               <svg className="bi" width="24" height="24" role="img" aria-label="Customers"></svg>
-            </a>
+            </Link>
           </li>
         </ul>
         <div className="dropdown border-top">
-          <a href="#" className="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle" id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
+          <Link href="#" className="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle" id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
             <img src="https://github.com/mdo.png" alt="mdo" width="24" height="24" className="rounded-circle" />
-          </a>
+          </Link>
           <ul className="dropdown-menu text-small shadow" aria-labelledby="dropdownUser3">
-            <li><a className="dropdown-item" href="#">New project...</a></li>
-            <li><a className="dropdown-item" href="#">Settings</a></li>
-            <li><a className="dropdown-item" href="#">Profile</a></li>
+            <li><Link className="dropdown-item" href="#">New project...</Link></li>
+            <li><Link className="dropdown-item" href="#">Settings</Link></li>
+            <li><Link className="dropdown-item" href="#">Profile</Link></li>
             <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="#">Sign out</a></li>
+            <li><Link className="dropdown-item" href="#">Sign out</Link></li>
           </ul>
         </div>
       </div>
