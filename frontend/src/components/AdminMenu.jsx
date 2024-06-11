3790e179be2a682e9e4833f37234fb53b7d6fa57
Tushar5201 <tusharlakadiya@gmail.com>
2024-06-11 09:13:48 +0530

    admin menu

--- frontend/src/components/AdminMenu.jsx
+++ frontend/src/components/AdminMenu.jsx
@@ -15,33 +15,33 @@ export default function AdminMenu() {
       </div> */}
       <div className="d-flex flex-column flex-shrink-0 bg-light" style="width: 4.5rem;">
         <a href="/" className="d-block p-3 link-dark text-decoration-none" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Icon-only">
-          <svg className="bi" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+          <svg className="bi" width="40" height="32"></svg>
           <span className="visually-hidden">Icon-only</span>
         </a>
         <ul className="nav nav-pills nav-flush flex-column mb-auto text-center">
           <li className="nav-item">
             <a href="#" className="nav-link active py-3 border-bottom" aria-current="page" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
-              <svg className="bi" width="24" height="24" role="img" aria-label="Home"><use xlink:href="#home"></use></svg>
+              <svg className="bi" width="24" height="24" role="img" aria-label="Home"></svg>
             </a>
           </li>
           <li>
             <a href="#" className="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
-              <svg className="bi" width="24" height="24" role="img" aria-label="Dashboard"><use xlink:href="#speedometer2"></use></svg>
+              <svg className="bi" width="24" height="24" role="img" aria-label="Dashboard"></svg>
             </a>
           </li>
           <li>
             <a href="#" className="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Orders">
-              <svg className="bi" width="24" height="24" role="img" aria-label="Orders"><use xlink:href="#table"></use></svg>
+              <svg className="bi" width="24" height="24" role="img" aria-label="Orders"></svg>
             </a>
           </li>
           <li>
             <a href="#" className="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Products">
-              <svg className="bi" width="24" height="24" role="img" aria-label="Products"><use xlink:href="#grid"></use></svg>
+              <svg className="bi" width="24" height="24" role="img" aria-label="Products"></svg>
             </a>
           </li>
           <li>
             <a href="#" className="nav-link py-3 border-bottom" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Customers">
-              <svg className="bi" width="24" height="24" role="img" aria-label="Customers"><use xlink:href="#people-circle"></use></svg>
+              <svg className="bi" width="24" height="24" role="img" aria-label="Customers"></svg>
             </a>
           </li>
         </ul>
